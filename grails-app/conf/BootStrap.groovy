c5ca84f4b285c2561f280f62dd3bad20225fab3e
Jeff Scott Brown <jeff@jeffandbetsy.net>
2015-08-25 21:57:24 -0500

    fix password

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,6 @@
 import demo.security.*
 
 class BootStrap {
-    def springSecurityService
 
     def init = { servletContext ->
         if (!User.count()) {
@@ -12,10 +11,8 @@ class BootStrap {
     private void createData() {
         def userRole = new Role(authority: 'ROLE_USER').save(failOnError: true)
 
-        String password = springSecurityService.encodePassword('password')
-
         ['jeff', 'jason'].each { userName ->
-            def user = new User(username: userName, password: password, enabled: true).save(failOnError: true)
+            def user = new User(username: userName, password: 'password', enabled: true).save(failOnError: true)
             UserRole.create user, userRole, true
         }
     }
